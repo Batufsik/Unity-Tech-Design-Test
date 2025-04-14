c5c9879374b5fe0a92adedd2275fdd500bf7545d
Batufsik <174842037+Batufsik@users.noreply.github.com>
2025-04-14 20:28:12 +1000

    feat: Add menu audio
    - Added menu music and button click sound

--- Assets/Scripts/UI_FUNCTIONS.cs
+++ Assets/Scripts/UI_FUNCTIONS.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class UI_FUNCTIONS : MonoBehaviour
 {
+    public AudioSource sound;
+    public AudioClip clip;
     public void Menu_Start()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        sound.Play();
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Menu_Exit()
     {
         Application.Quit();
     }
+
 }
